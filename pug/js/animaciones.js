77e359c417e02711f217986a9e539b99d9ef4f05
Javier <javiermarin798@gmail.com>
2021-11-08 21:41:29 +0100

    animaciones

--- pug/js/animaciones.js
+++ pug/js/animaciones.js
@@ -1,10 +1,11 @@
-function clickCuadrado(item) {    
-    if(item.className=="animacion1"){
-        item.className="animacion2";
-    }else{
-        item.className="animacion1";
+function clickCuadrado(item) {
+    if (item.className == "animacion1") {
+        item.className = "animacion2";
+
+    } else {
+        item.className = "animacion1";
         setTimeout(() => {
-            item.className="animacion2";
+            item.className = "animacion2";
         }, 6000);
-    }    
+    }
 }
