03e6dabf115d279e4ff77b63e2fb42eb66c8d0ee
Javier <javiermarin798@gmail.com>
2021-11-25 20:51:15 +0100

    dado

---
+++ pug/js/dado.js
@@ -0,0 +1,9 @@
+ var numeros = [1,2,3,4,5,6];
+ var numero=Math.random() * (numero.length);
+
+ document.getElementById("superior").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
+ document.getElementById("frente").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
+ document.getElementById("derecha").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
+ document.getElementById("izquierda").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
+ document.getElementById("atras").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
+ document.getElementById("inferior").style.backgroundImage ="url(./img/"+parseInt(numero)+".png)";
